b5ca742cb171e67fb86a551738739fc7f78de73d
ih_____________eb <58560500+ihebXxxjaouadi@users.noreply.github.com>
2023-10-29 23:59:19 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import { useCallback} from 'react';
+import { View, StyleSheet } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import DarkModeView from './Views/darkModeView';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'Montserrat-Regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+    'Montserrat-Medium': require('./assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-SemiBold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <DarkModeView />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+  },
+});
---
+++ components/smart_watch.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function smart_watch() {
+  return (
+    <View>
+      <Text>smart_watch</Text>
+    </View>
+  )
+}
